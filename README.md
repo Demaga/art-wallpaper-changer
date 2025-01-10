781ca6ed18681aa47ea909bfff6c70ff06d2dc18
Bohdan Kholodenko <bohdan.kholodenko@gmail.com>
2025-01-10 15:09:49 +0300

    Move temporary files to other directory to allow running on startup without opening images

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This script replaces your wallpaper with random art piece (and includes title, a
 
 Works for Windows only. Intended to run on system startup to replace wallpaper daily.
 
+Bundled to exe with PyInstaller. Command to bundle: `pyinstaller --noconsole --onefile main.py`
+
 # Credits
 
 Thanks to Metropolitan Art Museum for provided images and API! 
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import ctypes
 import os
-import random
 
 import requests
-from PIL import Image
 
 from met import get_random_art
 from util import add_text_to_image, resize_image_to_screen_width
+
+img_path = os.environ["USERPROFILE"] + "/tmp.jpeg"
 
 art = get_random_art()
 img_url = art["primaryImage"]
 print(img_url)
 
 res = requests.get(img_url)
-with open("tmp.jpeg", "wb") as f:
+with open(img_path, "wb") as f:
     f.write(res.content)
 
 
 # resize image
-input_image = "./tmp.jpeg"
-output_image = "./tmp_edited.jpeg"
+input_image = img_path
+output_image = img_path.replace("tmp", "tmp_edited")
 resize_image_to_screen_width(input_image, output_image)
 text = f"""Title: {art.get('title')}
 Culture: {art.get('culture')}
@@ -30,6 +30,6 @@ Date: {art.get('objectDate')}
 add_text_to_image(output_image, text)
 
 # set wallpaper in Windows
-wallpaper_path = "./tmp_edited.jpeg"
+wallpaper_path = output_image
 abs_path = os.path.abspath(wallpaper_path)
 ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_path, 0)
